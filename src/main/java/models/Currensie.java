c4468939061c7a63011022c23155976023702506
j_dim <dmitrijsuplev@gmail.com>
2025-06-18 14:01:39 +0300

    create_models

---
+++ src/main/java/models/Currensie.java
@@ -0,0 +1,14 @@
+package models;
+
+public class Currensie {
+    private int id;
+    private String code;
+    private String fullName;
+    private String signe;
+
+
+    @Override
+    public String toString() {
+        return "id : " + id;
+    }
+}
---
+++ src/main/java/models/ExchangeRates.java
@@ -0,0 +1,10 @@
+package models;
+
+import java.math.BigDecimal;
+
+public class ExchangeRates {
+ private int id;
+ private Currensie baseCurrency;
+ private Currensie targetCurency;
+ private BigDecimal rate;
+}
