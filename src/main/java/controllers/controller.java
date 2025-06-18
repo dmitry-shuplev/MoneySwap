3a64f004bcaec76dcb597f596414f86fdf502bdc
j_dim <dmitrijsuplev@gmail.com>
2025-06-18 17:37:34 +0300

    add_controller_currencies

---
+++ src/main/java/controllers/controller.java
@@ -0,0 +1,10 @@
+package controllers;
+
+public abstract class controller {
+
+    public void create(){}
+
+    public void upate(){}
+
+    public void delete(){}
+}
---
+++ src/main/java/controllers/currenciesController.java
@@ -0,0 +1,19 @@
+package controllers;
+
+public class currenciesController extends controller{
+
+    @Override
+    public void create() {
+        super.create();
+    }
+
+    @Override
+    public void upate() {
+        super.upate();
+    }
+
+    @Override
+    public void delete() {
+        super.delete();
+    }
+}
