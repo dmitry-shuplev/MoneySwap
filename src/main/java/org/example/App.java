302cc800333090c69b4b4df7550718f0faa62090
j_dim <dmitrijsuplev@gmail.com>
2025-06-18 14:02:41 +0300

    check_database_connection

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,17 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.sql.*;
+
+public class App {
+    public static void main(String[] args) {
+
+        try {
+            String url = "jdbc:sqlite:src/main/resources/money.db";
+            Connection connection = DriverManager.getConnection(url);
+            System.out.println("Успешнo подключено");
+        } catch (Exception e) {
+            System.out.println("Ошибка подключения: " + e.getMessage());
+        }
     }
 }
+
