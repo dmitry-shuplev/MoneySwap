3329047594526575811667cbd827e93d8855adfe
j_dim <dmitrijsuplev@gmail.com>
2025-06-30 17:19:15 +0300

    add_test_servlet

---
+++ src/main/java/servlets/TestServlet.java
@@ -0,0 +1,29 @@
+package servlets;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/test")
+public class TestServlet extends HttpServlet {
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Test Servlet is working!</h1>");
+    }
+
+    @Override
+    public void destroy(){
+        super.destroy();
+    }
+}
