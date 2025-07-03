39bd14752861c16ff8567f8a4ac23df79ba0c2a8
”Dmitry <dmitrijsuplev@gmail.com>
2025-07-03 15:10:53 +0300

    currensies_servlet_supplemented

--- src/main/java/servlets/CurrenciesServlet.java
+++ src/main/java/servlets/CurrenciesServlet.java
@@ -1,4 +1,39 @@
 package servlets;
 
-public class CurrenciesServlet {
+import jakarta.servlet.Servlet;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/currencies")
+public class CurrenciesServlet extends HttpServlet {
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("application/json");
+        PrintWriter out = response.getWriter();
+        out.println("Servlet currencies method get is connected");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response){
+
+    }
+
+    @Override
+    public void destroy(){super.destroy();}
+
+
+
 }
