0c99e272e3936a161e937fb85a7a18cff0b284bf
j_dim <dmitrijsuplev@gmail.com>
2025-07-09 18:17:20 +0300

    broke_sql_connection_in_tomcat

--- src/main/java/models/DBUtils.java
+++ src/main/java/models/DBUtils.java
@@ -1,15 +1,35 @@
 package models;
 
-import config.DataSourceConfig;
+import org.apache.commons.dbcp2.BasicDataSource;
 
 import java.sql.Connection;
 import java.sql.SQLException;
-import javax.sql.DataSource;
 
 public class DBUtils {
-    public static Connection getConnection() throws SQLException {
-        DataSource ds = DataSourceConfig.getDataSource();
-        return ds.getConnection();
+    private final static String SQLITE_JDBC_DRIVER = "org.sqlite.JDBC";
+    private final static String DATABASE_URL = "jdbc:sqlite::resource:money.db";
+    private final static int MIN_MINUTES_IDLE = 5;
+    private final static int MAX_MINUTES_IDLE = 10;
+    private final static int MAX_OPENED_CONNECTIONS = 20;
 
+    private static BasicDataSource dataSource = new BasicDataSource();
+    //log
+    int i = 10;
+
+    static {
+        dataSource.setUrl(DATABASE_URL);
+        dataSource.setDriverClassName(SQLITE_JDBC_DRIVER);
+        dataSource.setMinIdle(MIN_MINUTES_IDLE);
+        dataSource.setMaxIdle(MAX_MINUTES_IDLE);
+        dataSource.setMaxOpenPreparedStatements(MAX_OPENED_CONNECTIONS);
+    }
+
+    public static Connection getConnection() throws SQLException {
+
+
+        return dataSource.getConnection();
+    }
+
+    private DBUtils() {
     }
 }
--- src/main/java/models/dao/CurrencyDao.java
+++ src/main/java/models/dao/CurrencyDao.java
@@ -2,6 +2,7 @@ package models.dao;
 
 import models.Currensie;
 import models.DBUtils;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -11,15 +12,16 @@ public class CurrencyDao {
     public Currensie getByCode(String code) {
         String queryTemplate = "SELECT * FROM Currencies WHERE Code = ?";
         Currensie currency = new Currensie();
-        try (Connection connection = DBUtils.getConnection();
-             PreparedStatement pstmt = connection.prepareStatement(queryTemplate)) {
+        try {
+            Connection connection = DBUtils.getConnection();
+            PreparedStatement pstmt = connection.prepareStatement(queryTemplate);
             pstmt.setString(1, code);
             ResultSet result = pstmt.executeQuery();
             if (result.next()) {
-               currency.setId(result.getInt("Id"));
-               currency.setCode(result.getString("Code"));
-               currency.setFullName(result.getString("FullName"));
-               currency.setSign(result.getString("Sign"));
+                currency.setId(result.getInt("Id"));
+                currency.setCode(result.getString("Code"));
+                currency.setFullName(result.getString("FullName"));
+                currency.setSign(result.getString("Sign"));
             }
         } catch (Exception e) {
             e.printStackTrace();
