fdc5717ef6c41fd7519b82b8e74ebb5666027cdd
j_dim <dmitrijsuplev@gmail.com>
2025-07-17 09:45:11 +0300

    chore:add_lombok,rewrite_currensies_with_it

--- src/main/java/models/Currensies.java
+++ src/main/java/models/Currensies.java
@@ -1,5 +1,10 @@
 package models;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Currensies {
     private int id;
     private String code;
@@ -8,41 +13,7 @@ public class Currensies {
 
 
     public Currensies(){
-
     }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getSign() {
-        return sign;
-    }
-
-    public void setSign(String sign) {
-        this.sign = sign;
-    }
-
 
 
     @Override
